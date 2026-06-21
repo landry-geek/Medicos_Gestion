9acb30c8044a903176655d84055d7ab690d658d2
PetiteJava <herilalainaandriamiora@gmail.com>
2026-06-21 09:50:45 +0300

    creating of the side bar

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
-    <div>
-        <h1>Hey Bienvenu dans le projet !!! </h1>
-        <div>test fotsiny ito</div>
+    <div class="bg-gradient-to-r from-purple-400 to-blue-300 w-screen h-screen flex justify-between">
+        <div class="bg-gray-800/90 w-50 h-screen rounded-tr-3xl rounded-br-3xl">
+            <h1 class="text-center  text-white text-3xl font-bold drop-shadow-lg">MEDICOS APP</h1>     
+            
+            <div class="text-center space-y-2 pt-35" >
+            <div class="px-4 py-3 text-gray-300 hover:text-white hover:bg-white/10 rounded-lg transition duration-200 cursor-pointer">formulaire</div>
+            <div class="px-4 py-3 text-gray-300 hover:text-white hover:bg-white/10 rounded-lg transition duration-200 cursor-pointer">liste</div>
+            <div class="px-4 py-3 text-gray-300 hover:text-white hover:bg-white/10 rounded-lg transition duration-200 cursor-pointer">bilan</div>
+             
+            </div>
+        </div>
+        
+            
+        
+
+        <div class="bg-gradient-to-r from-purple-400 to-blue-300  w-300 h-screen"> 
+            <h1></h1>
+        </div>
     </div>
+   
 </template>
 
-<style scoped></style>
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
